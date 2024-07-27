120264272252451ba7405f35e0f8c6e34c224a13
Aleksandar Jovanovski <jovanovski.a74@gmail.com>
2024-07-27 15:13:10 +0200

    typewriter done

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-type", {
+  strings: ["a Painter.", "a Visual Artist.", "Creative :)"],
+  startDelay: 700,
+  backDelay: 700,
+  smartBackspace: true,
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
