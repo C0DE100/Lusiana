c0c6db35f10fa07b5bf71c2b4bf47882580d29ab
Aleksandar Jovanovski <aleksandar.jovanovski@generadevelopment.com>
2024-07-29 20:15:56 +0200

    homepage upgraded, slider added

--- owlCarousel.js
+++ owlCarousel.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-      items: 1,
-      margin: 10,
       loop: true,
       autoplay: true,
-      lazyLoad: true
+      lazyLoad: true,
+      autoplayTimeout: 3000,
+      responsiveClass:true,
+      responsive : {
+        // breakpoint from 0 up
+        0 : {
+          items: 1,
+          nav: true,
+          navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+        },
+        // breakpoint from 480 up
+        480 : {
+          items: 2,
+          margin: 30,
+        }
+    }
     });
   });
